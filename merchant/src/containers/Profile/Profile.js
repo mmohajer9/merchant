f755dcad7148329ae66c43a160993c0eca274dc4
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2021-06-05 19:25:26 +0430

    feat:profile is completed

--- merchant/src/containers/Profile/Profile.js
+++ merchant/src/containers/Profile/Profile.js
@@ -14,6 +14,8 @@ import {
   ListSubheader,
   makeStyles,
   Typography,
+  Divider,
+  Button,
 } from '@material-ui/core';
 import AccountCircleIconOutlined from '@material-ui/icons/AccountCircleOutlined';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
@@ -22,7 +24,9 @@ import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import ModeCommentOutlinedIcon from '@material-ui/icons/ModeCommentOutlined';
 import EmailOutlinedIcon from '@material-ui/icons/EmailOutlined';
 import LocalMallOutlinedIcon from '@material-ui/icons/LocalMallOutlined';
-import PermIdentityOutlinedIcon from '@material-ui/icons/PermIdentityOutlined';
+import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
+import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
+import ErrorOutlineOutlinedIcon from '@material-ui/icons/ErrorOutlineOutlined';
 import { ExpandLess, ExpandMore } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
@@ -38,6 +42,22 @@ const useStyles = makeStyles((theme) => ({
   listItemIcon: {
     color: 'inherit',
   },
+  logout: {
+    color: theme.palette.secondary.main,
+  },
+  personalInfo: {
+    marginBottom: theme.spacing(3),
+  },
+  personalInfoItem: {
+    marginBottom: theme.spacing(2),
+  },
+  cardActions: {
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
+  noPurchaseIcon : {
+    fontSize : theme.typography.fontSize * 7.25
+  }
 }));
 
 const Profile = (props) => {
@@ -45,9 +65,15 @@ const Profile = (props) => {
   const [profileOptionsOpen, setProfileOptionsOpen] = React.useState(true);
 
   return (
-    <Box mt={3}>
+    <Box mt={3} mb={5}>
       <Container maxWidth="xl">
-        <Grid spacing={3} container direction="row" justify="space-between" alignItems="flex-start">
+        <Grid
+          spacing={3}
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="flex-start"
+        >
           <Grid item container direction="column" xs={12} sm={4}>
             <Card>
               <CardContent>
@@ -65,17 +91,30 @@ const Profile = (props) => {
                   <ListItem
                     className={classes.listItem}
                     button
-                    onClick={() => setProfileOptionsOpen((prevState) => !prevState)}
+                    onClick={() =>
+                      setProfileOptionsOpen((prevState) => !prevState)
+                    }
                   >
                     <ListItemIcon className={classes.listItemIcon}>
                       <AccountCircleIconOutlined fontSize="large" />
                     </ListItemIcon>
-                    <ListItemText primary="Mohammad Mahdi Mohajer" secondary="09115269909" />
+                    <ListItemText
+                      primary="Mohammad Mahdi Mohajer"
+                      secondary="09115269909"
+                    />
                     {profileOptionsOpen ? <ExpandLess /> : <ExpandMore />}
                   </ListItem>
-                  <Collapse in={profileOptionsOpen} timeout="auto" unmountOnExit>
+
+                  <Collapse
+                    in={profileOptionsOpen}
+                    timeout="auto"
+                    unmountOnExit
+                  >
                     <List component="div" disablePadding>
-                      <ListItem button className={[classes.nested, classes.listItem]}>
+                      <ListItem
+                        button
+                        className={[classes.nested, classes.listItem]}
+                      >
                         <ListItemIcon className={classes.listItemIcon}>
                           <AccountBalanceWalletOutlinedIcon />
                         </ListItemIcon>
@@ -83,57 +122,172 @@ const Profile = (props) => {
                       </ListItem>
                     </List>
                   </Collapse>
-                  <ListItem className={classes.listItem} button>
-                    <ListItemIcon className={classes.listItemIcon}>
-                      <LocalMallOutlinedIcon fontSize="large" />
-                    </ListItemIcon>
-                    <ListItemText primary="Purchases" />
-                  </ListItem>
+                  <Divider />
+                  <Box clone mt={3}>
+                    <ListItem className={classes.listItem} button>
+                      <ListItemIcon className={classes.listItemIcon}>
+                        <LocalMallOutlinedIcon fontSize="large" />
+                      </ListItemIcon>
+                      <ListItemText primary="Purchases" />
+                    </ListItem>
+                  </Box>
                   <ListItem className={classes.listItem} button>
                     <ListItemIcon className={classes.listItemIcon}>
                       <FavoriteBorderIcon fontSize="large" />
                     </ListItemIcon>
                     <ListItemText primary="Favorites" />
                   </ListItem>
-                  <ListItem gutt className={classes.listItem} button>
+                  <ListItem className={classes.listItem} button>
                     <ListItemIcon className={classes.listItemIcon}>
                       <ModeCommentOutlinedIcon fontSize="large" />
                     </ListItemIcon>
                     <ListItemText primary="Comments" />
                   </ListItem>
-                  <ListItem gutt className={classes.listItem} button>
+                  <ListItem className={classes.listItem} button>
                     <ListItemIcon className={classes.listItemIcon}>
                       <LocationOnOutlinedIcon fontSize="large" />
                     </ListItemIcon>
                     <ListItemText primary="Addresses" />
                   </ListItem>
-                  <ListItem gutt className={classes.listItem} button>
+                  <ListItem className={classes.listItem} button>
                     <ListItemIcon className={classes.listItemIcon}>
                       <EmailOutlinedIcon fontSize="large" />
                     </ListItemIcon>
                     <ListItemText primary="Message Inbox" />
                   </ListItem>
-
-                  <ListItem gutt className={classes.listItem} button>
+                  <ListItem className={classes.listItem} button>
                     <ListItemIcon className={classes.listItemIcon}>
-                      <PermIdentityOutlinedIcon fontSize="large" />
+                      <SettingsOutlinedIcon fontSize="large" />
                     </ListItemIcon>
                     <ListItemText primary="Personal Preferences" />
                   </ListItem>
                 </List>
               </CardContent>
-              <CardActions></CardActions>
+              <Divider />
+              <CardActions>
+                <ListItem className={classes.logout} button>
+                  <ListItemIcon>
+                    <ExitToAppOutlinedIcon color="secondary" fontSize="large" />
+                  </ListItemIcon>
+                  <ListItemText primary="Log Out" />
+                </ListItem>
+              </CardActions>
             </Card>
           </Grid>
-          <Grid item container direction="column" xs={12} sm={8}>
-            <Card>
-              <CardContent>
-                <Typography color="" gutterBottom>
-                  Word of the Day
-                </Typography>
-              </CardContent>
-              <CardActions></CardActions>
-            </Card>
+          <Grid item container direction="row" xs={12} sm={8}>
+            <Grid item className={classes.personalInfo} xs={12}>
+              <Card>
+                <CardContent>
+                  <Typography color="textPrimary" gutterBottom paragraph>
+                    Personal Information
+                  </Typography>
+                  <Grid container direction="row">
+                    <Grid item className={classes.personalInfoItem} xs={6}>
+                      <Typography
+                        color="textSecondary"
+                        variant="subtitle1"
+                        gutterBottom
+                      >
+                        Full Name
+                      </Typography>
+                      <Typography color="textPrimary" variant="h6" gutterBottom>
+                        Mohammad Mahdi Mohajer
+                      </Typography>
+                    </Grid>
+                    <Grid item className={classes.personalInfoItem} xs={6}>
+                      <Typography
+                        color="textSecondary"
+                        variant="subtitle1"
+                        gutterBottom
+                      >
+                        Email
+                      </Typography>
+                      <Typography color="textPrimary" variant="h6" gutterBottom>
+                        mohajer@ec.iut.ac.ir
+                      </Typography>
+                    </Grid>
+                    <Grid item className={classes.personalInfoItem} xs={6}>
+                      <Typography
+                        color="textSecondary"
+                        variant="subtitle1"
+                        gutterBottom
+                      >
+                        Phone Number
+                      </Typography>
+                      <Typography color="textPrimary" variant="h6" gutterBottom>
+                        09115269909
+                      </Typography>
+                    </Grid>
+                    <Grid item className={classes.personalInfoItem} xs={6}>
+                      <Typography
+                        color="textSecondary"
+                        variant="subtitle1"
+                        gutterBottom
+                      >
+                        National ID
+                      </Typography>
+                      <Typography color="textPrimary" variant="h6" gutterBottom>
+                        2110772859
+                      </Typography>
+                    </Grid>
+                    <Grid item className={classes.personalInfoItem} xs={6}>
+                      <Typography
+                        color="textSecondary"
+                        variant="subtitle1"
+                        gutterBottom
+                      >
+                        Account Status
+                      </Typography>
+                      <Typography color="textPrimary" variant="h6" gutterBottom>
+                        Activated
+                      </Typography>
+                    </Grid>
+                    <Grid item className={classes.personalInfoItem} xs={6}>
+                      <Typography
+                        color="textSecondary"
+                        variant="subtitle1"
+                        gutterBottom
+                      >
+                        Account Verified
+                      </Typography>
+                      <Typography color="textPrimary" variant="h6" gutterBottom>
+                        Verified
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+                <CardActions className={classes.cardActions}>
+                  <Button fullWidth variant="outlined" size="large">
+                    Edit Personal Information
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item className={classes.personalInfo} xs={12}>
+              <Card>
+                <CardContent>
+                  <Typography color="textPrimary" gutterBottom>
+                    Latest Purchases
+                  </Typography>
+                  {/* <Grid container></Grid> */}
+                  <Grid container direction="column" alignItems="center">
+                    <Grid item className={classes.noPurchaseIcon}>
+                      <ErrorOutlineOutlinedIcon fontSize="inherit" />
+                    </Grid>
+                    <Grid item className={classes.noPurchaseText}>
+                      <Typography color="textPrimary" gutterBottom>
+                        You Have No Any Recent Purchases
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+                {/* <CardActions className={classes.cardActions}>
+                  <Button fullWidth variant="outlined" size="large">
+                    See All of The Purchases
+                  </Button>
+                </CardActions> */}
+              </Card>
+            </Grid>
           </Grid>
         </Grid>
       </Container>
