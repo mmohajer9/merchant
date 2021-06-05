368d44f6111d0f28a4797891cb3e9b08f3c4baad
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2021-06-05 14:41:15 +0430

    refactor:options code refactored

--- merchant/src/components/Header/fragments/OptionsToolbar.js
+++ merchant/src/components/Header/fragments/OptionsToolbar.js
@@ -1,36 +1,28 @@
-import React from "react";
-import { fade, useTheme, withStyles } from "@material-ui/core/styles";
-import Menu from "@material-ui/core/Menu";
+import React from 'react';
+import { fade, useTheme, withStyles } from '@material-ui/core/styles';
+import Menu from '@material-ui/core/Menu';
 
-import MenuOutlinedIcon from "@material-ui/icons/MenuOutlined";
-import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
-import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
-import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+import MenuOutlinedIcon from '@material-ui/icons/MenuOutlined';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 
-import MegaMenu from "./MegaMenu";
-import {
-  Box,
-  InputBase,
-  makeStyles,
-  Tab,
-  Tabs,
-  Toolbar,
-  useMediaQuery,
-} from "@material-ui/core";
-import { Link } from "react-router-dom";
-import routes from "../../../common/routes";
+import MegaMenu from './MegaMenu';
+import { Box, InputBase, makeStyles, Tab, Tabs, Toolbar, useMediaQuery } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import routes from '../../../common/routes';
 
 const StyledMenu = withStyles({})((props) => (
   <Menu
     elevation={5}
     getContentAnchorEl={null}
     anchorOrigin={{
-      vertical: "bottom",
-      horizontal: "center",
+      vertical: 'bottom',
+      horizontal: 'center',
     }}
     transformOrigin={{
-      vertical: "top",
-      horizontal: "center",
+      vertical: 'top',
+      horizontal: 'center',
     }}
     {...props}
   />
@@ -38,62 +30,62 @@ const StyledMenu = withStyles({})((props) => (
 
 const useStyles = makeStyles((theme) => ({
   inlineTab: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   tab: {
-    marginRight: "1rem",
-    marginLeft: "1rem",
+    marginRight: '1rem',
+    marginLeft: '1rem',
     minWidth: 30,
     padding: 0,
-    textTransform: "none",
-    "& svg": {
-      fontSize: "2rem",
-      marginRight: "0.5rem",
+    textTransform: 'none',
+    '& svg': {
+      fontSize: '2rem',
+      marginRight: '0.5rem',
     },
   },
   tabContainer: {
-    marginLeft: "auto",
+    marginLeft: 'auto',
   },
   search: {
-    position: "relative",
+    position: 'relative',
     borderRadius: theme.shape.borderRadius + 3,
     borderColor: fade(theme.palette.common.grey, 0.4),
-    borderStyle: "solid",
+    borderStyle: 'solid',
     backgroundColor: fade(theme.palette.common.lightGrey, 1),
-    "&:hover": {
+    '&:hover': {
       borderColor: fade(theme.palette.common.grey, 1),
     },
-    marginRight: "auto",
+    marginRight: 'auto',
     marginLeft: theme.spacing(2),
-    width: "80%",
+    width: '80%',
   },
   searchIcon: {
     padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   inputRoot: {
-    color: "inherit",
-    width: "100%",
+    color: 'inherit',
+    width: '100%',
   },
   inputInput: {
     padding: theme.spacing(1.2, 1, 1.2, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
+    transition: theme.transitions.create('width'),
+    width: '100%',
   },
 }));
 
 export default function CustomizedMenus() {
   const classes = useStyles();
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.down("sm"));
+  const matches = useMediaQuery(theme.breakpoints.down('sm'));
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [value, setValue] = React.useState(0);
 
@@ -122,7 +114,7 @@ export default function CustomizedMenus() {
               root: classes.inputRoot,
               input: classes.inputInput,
             }}
-            inputProps={{ "aria-label": "search" }}
+            inputProps={{ 'aria-label': 'search' }}
           />
         </Box>
         <Tab
