cecd4d2009a03d92da29720ea34ea1541a3da0c0
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2021-06-16 19:20:23 +0430

    feat:website configuration has been completed

--- backend/application/admin.py
+++ backend/application/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import WebsiteConfiguration
 
 # Register your models here.
+@admin.register(WebsiteConfiguration)
+class WebsiteConfigurationAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "created_at", "updated_at")
+    # list_filter = ["user", "title", "business_phone"]
+    # search_fields = ('user__username','postal_code__startswith')
+    # prepopulated_fields = {'slug': ('title',)}
--- backend/application/models.py
+++ backend/application/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+
+class WebsiteConfiguration(models.Model):
+    name = models.CharField(max_length=500, verbose_name=_("Name"))
+    description = models.CharField(
+        max_length=500, blank=True, null=True, verbose_name=_("Description")
+    )
+    # -------------------------------------------------------------------------
+
+    # put your desired settings in here
+
+    # -------------------------------------------------------------------------
+    created_at = models.DateTimeField(
+        _("Created at"), auto_now_add=True, blank=True, null=True
+    )
+    updated_at = models.DateTimeField(
+        _("Updated at"), auto_now=True, blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        # db_table = ""
+        # managed = True
+        verbose_name = "Website Configuration"
+        verbose_name_plural = "Website Configurations"
