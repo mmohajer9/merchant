6ec0e0e3e2f26b483d95f97872cfaf1d8f923743
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2021-06-18 17:07:25 +0430

    feat:Forgotpassword redux integration is now completed

--- merchant/src/store/auth/forgotpassword.js
+++ merchant/src/store/auth/forgotpassword.js
@@ -1,13 +1,36 @@
 import routes from '../../common/routes';
 import axiosInstance from '../../common/axios';
 
-import { authActions } from '.';
 import { toast } from 'react-toastify';
 
 export const forgotPasswordAction = ({ values, history }) => {
   return async (dispatch) => {
-    const path = routes.api.registration.path;
-    const payload = routes.api.registration.payload;
+    const path = routes.api.resetPassword.path;
+    const payload = routes.api.resetPassword.payload;
+    payload.email = values.email;
+    try {
+      const { data } = await axiosInstance.post(path, payload);
+      toast.success(data.detail, {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
+    } catch (error) {
+      const errorMessages = error.response.data;
+      console.log(errorMessages);
+      toast.error('Operation failed, Try again', {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
   };
 };
 
