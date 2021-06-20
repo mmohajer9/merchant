cf9719d4a110af9b9180340037fa4401709fe86b
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2021-06-21 01:02:07 +0430

    feat:order create is now capabale of writable nested which means i can pass products to be added as order items to the order creation api only post request is valid + put and patch for aborting and finishing the orders

--- backend/shop/serializers.py
+++ backend/shop/serializers.py
@@ -1,4 +1,10 @@
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import (
+    ErrorDetail,
+    ValidationError as RestValidationError,
+)
+from django.db.models import F
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.conf import settings
@@ -44,12 +50,29 @@ class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = "__all__"
-        read_only_fields = ["id", "order", "created_at", "updated_at"]
+        read_only_fields = [
+            "id",
+            "order",
+            "price",
+            "product",
+            "created_at",
+            "updated_at",
+        ]
+
+
+class OrderProductSerializer(serializers.ModelSerializer):
+
+    product_id = serializers.IntegerField()
+
+    class Meta:
+        model = Product
+        fields = ("product_id", "quantity")
 
 
 class OrderSerializer(serializers.ModelSerializer):
 
-    orderitem_set = OrderItemSerializer(many=True)
+    orderitem_set = OrderItemSerializer(many=True, read_only=True)
+    products = OrderProductSerializer(many=True, write_only=True)
 
     class Meta:
         model = Order
@@ -60,4 +83,74 @@ class OrderSerializer(serializers.ModelSerializer):
             "updated_at",
             "user_id",
             "orderitem_set",
+            "products",
         )
+
+    def create(self, validated_data):
+        products_data = validated_data.pop("products")
+
+        try:
+            (order, created) = Order.objects.get_or_create(
+                user=validated_data.pop("user"), status="pending"
+            )
+        except:
+            order = Order.objects.filter(
+                user=validated_data.pop("user"), status="pending"
+            ).latest("updated_at")
+        try:
+            for product_data in products_data:
+                properties = product_data.items()
+                iterable = iter(properties)
+                for item in iterable:
+                    global product_id
+                    global quantity
+                    global order_items
+                    order_items = []
+                    product_id = item[1]
+                    quantity = next(iterable)[1]
+                    # --------------------------------------------------------------
+                    product = Product.objects.get(pk=product_id)
+                    order_item = order.orderitem_set.filter(
+                        product__id=product_id
+                    ).first()
+                    # --------------------------------------------------------------
+                    """
+                        handle two common errors:
+                        ordering more than stock item quantity
+                        ordering when the product is not available (quantity = 0)
+                        if there were also the same item that you want to add to the order
+                        just update the quantity fields on the order items o/w add a new order item
+                    """
+                    if (
+                        (not product.is_available)
+                        or (product.quantity < quantity)
+                        or (
+                            order_item
+                            and product.quantity < quantity + order_item.quantity
+                        )
+                    ):
+                        raise Exception("not sufficient product in the stock")
+                    elif order_item:
+                        order_item.quantity += quantity
+                        order_items.append(order_item)
+                    else:
+                        new_order_item = OrderItem.objects.create(
+                            order=order,
+                            product=product,
+                            quantity=quantity,
+                            price=product.final_price(),
+                        )
+                    # --------------------------------------------------------------
+
+        except Exception as e:
+            raise RestValidationError(
+                {
+                    "error": {
+                        "product_id": product_id,
+                        "message": e,
+                    }
+                }
+            )
+
+        [order_item.save() for order_item in order_items]
+        return order
--- backend/shop/views.py
+++ backend/shop/views.py
@@ -1,6 +1,7 @@
+from rest_framework.response import Response
 from shop.filters import ProductFilter
 from django.db.models.query_utils import Q
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework import permissions
 
 from .serializers import OrderItemSerializer, OrderSerializer, ProductSerializer
@@ -62,6 +63,20 @@ class OrderViewSet(EnhancedModelViewSet):
     pagination_class = CustomLimitOffsetPagination
     serializer_class = OrderSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+    # override per action
+    action_permission_classes = {
+        "destroy": [Forbidden],
+    }
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
