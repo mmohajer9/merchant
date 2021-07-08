f76d81b9de6c0894bf62a26b07b7f9ea98ddd480
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2021-07-09 00:33:10 +0430

    fix:minor secret key

--- backend/config/settings/common.py
+++ backend/config/settings/common.py
@@ -30,7 +30,7 @@ MEDIA_DIR = BASE_DIR / "media"
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = config("SECRET_KEY", default=".")
+SECRET_KEY = config("SECRET_KEY", default=".")
 
 
 # Application definition
